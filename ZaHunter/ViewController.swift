a286cf068711bfeb50195d2023d09fb8e884e877
natalie <njurcik4503@stu.d214.org>
2021-05-14 15:20:21 -0500

    zahunter!

--- ZaHunter/ViewController.swift
+++ ZaHunter/ViewController.swift
@@ -7,14 +7,39 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
-    override func viewDidLoad() {
+    @IBOutlet weak var mapView: MKMapView!
+    var currentLocation:CLLocation!
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        locationManager.requestWhenInUseAuthorization()
+        mapView.showsUserLocation = true
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
     }
-
-
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
+    {
+        currentLocation = locations[0]
+    }
+    
+    @IBAction func whenZoomButtonPressed(_ sender: Any) {
+        
+//        let center = CLLocationCoordinate2D(latitude: currentLocation.coordinate.latitude, longitude: currentLocation.coordinate.longitude)
+        
+        let center = currentLocation.coordinate
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: center, span: span)
+        mapView.setRegion(region, animated: true)
+    }
 }
 
